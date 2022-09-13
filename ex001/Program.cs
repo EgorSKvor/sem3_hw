3b9995ef7b70c827987eb9563eef345f2e0195a5
Egor <ee3068316@gmail.com>
2022-09-13 12:33:55 +0300

    ex1

--- ex001/Program.cs
+++ ex001/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите 5ти значное число и магия определит является ли оно палиндромом");
+string p = Console.ReadLine();
+
+void CheckingNumber (string p)
+{
+    if( p[0] == p[4] || p[1] == p[3]){
+    System.Console.WriteLine($"Ваше число {p} - палиндром");
+}
+else 
+    System.Console.WriteLine($"Ваше число {p} - не палиндром");
+}
+if (p.Length == 5)
+{
+    CheckingNumber(p);
+}
+else
+{
+    System.Console.WriteLine("Введите правильное число");
+}
